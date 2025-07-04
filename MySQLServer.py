5c2ae27e33fe9be553dee02d7b7c10dbada5ed4c
Ibrahim <ibrahimsakariyah@gmail.com>
2025-07-04 01:01:33 +0100

    mysql db creation in python

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,17 +1,28 @@
 import mysql.connector
+from mysql.connector import Error
 
-mydb = mysql.connector.connect(
-     host = "localhost",
-    user="root",
-    passwd="SIbro@124"
-    # database="testdatabase"
-)
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user="root",
+        passwd="SIbro@124"
+        # database="testdatabase"
+    )
 
-myCursor = mydb.cursor()
+    myCursor = mydb.cursor()
 
-myCursor.execute("""CREATE DATABASE IF NOT EXISTS alx_book_store""")
+    myCursor.execute("""CREATE DATABASE IF NOT EXISTS alx_book_store""")
 
-print("Database 'alx_book_store' created successfully!")
+    print("Database 'alx_book_store' created successfully!")
 
-myCursor.close()
-mydb.close()
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+finally:
+    if 'myCursor' in locals() and myCursor:
+        myCursor.close()
+    if 'mydb' in locals() and mydb:
+        mydb.close()
+
+
+
